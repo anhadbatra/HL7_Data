4403ffdbdaa8fcfe3ae36c163df181cd15687ab0
anhadbatra <anhadbatra1998@gmail.com>
2025-07-10 23:58:49 -0400

    lamdas

--- lambdas/data.py
+++ lambdas/data.py
@@ -1,9 +1,26 @@
 import requests
 import json
 import boto3
+import datetime
 
-data = requests.get("https://hapi.fhir.org/baseR4/Patient?_count=10")
 
-data = data.json()
+s3_bucket = "neurosytnx-raw"
+s3 = boto3.client('s3')
+FHIR_API = "https://hapi.fhir.org/baseR4/Patient?_count=10"
 
-print(data)
+def get_data():
+    data = requests.get(FHIR_API)
+    data = data.json()
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    s3_key = f"fhir_data/patients_{timestamp}.json"
+
+    s3.put_objects(
+        BUCKET = s3_bucket,
+        KEY = s3_key,
+        Body = json.dumps(data),
+        ContentType = "application/json"
+    )
+
+
+if __name__ == "__main__":
+    get_data()
